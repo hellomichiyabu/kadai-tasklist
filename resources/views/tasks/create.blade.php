b18d81834a6ab02e30a7fdd89c548f74655d02c5
Yuta <hellomichiyabu@yahoo.com>
2017-02-07 12:12:05 +0000

    bootstrap design

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -5,18 +5,19 @@
     <h1>メッセージ新規作成ページ</h1>
 
     @include('commons.error_messages')
+    <div class="row">
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">    
+            {!! Form::model($task, ['route' => 'tasks.store']) !!}
 
+                {!! Form::label('content', 'タイトル:') !!}
+                {!! Form::text('content', null, ['class' => 'form-control']) !!}
 
-    {!! Form::model($task, ['route' => 'tasks.store']) !!}
+                {!! Form::label('status', 'ステータス:') !!}
+                {!! Form::text('status', null, ['class' => 'form-control']) !!}
 
-        {!! Form::label('content', 'タイトル:') !!}
-        {!! Form::text('content') !!}
+                {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
 
-        {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
-
-        {!! Form::submit('投稿') !!}
-
-    {!! Form::close() !!}
-
+            {!! Form::close() !!}
+        </div>
+    </div>
 @endsection
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -5,17 +5,19 @@
     <h1>id: {{ $task->id }} のメッセージ編集ページ</h1>
 
     @include('commons.error_messages')
-
-    {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
+    <div class="row">
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">  
+        {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
 
         {!! Form::label('content', 'タイトル:') !!}
-        {!! Form::text('content') !!}
+        {!! Form::text('content', null, ['class' => 'form-control']) !!}
 
         {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
+        {!! Form::text('status'), null, ['class' => 'form-control']) !!}
 
-        {!! Form::submit('更新') !!}
+        {!! Form::submit('更新', ['class' => 'btn btn-default']) !!}
 
     {!! Form::close() !!}
-
+        </div>
+    </div>
 @endsection
